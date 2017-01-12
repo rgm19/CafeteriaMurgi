a2e6407b32c97359708aac802e8dde1eb090b1bd
Gand <zaken85@gmail.com>
2017-01-12 17:00:40 +0100

    Gand - Creación de Activitys

--- app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/ActivityMenuConfirmacion.java
+++ app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/ActivityMenuConfirmacion.java
@@ -3,13 +3,11 @@ package a2dam.fila1.grupo.proyecto_trimestre_2_cafeteria;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import a2dam.fila1.grupo.proyecto_trimestre_2_cafeteria.R;
-
-public class MainActivity extends AppCompatActivity {
+public class ActivityMenuConfirmacion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu_confirmacion);
     }
 }
--- app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/ActivityMenuDetalles.java
+++ app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/ActivityMenuDetalles.java
@@ -1,15 +1,15 @@
 package a2dam.fila1.grupo.proyecto_trimestre_2_cafeteria;
 
+//Detalles del pedido selecionado
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import a2dam.fila1.grupo.proyecto_trimestre_2_cafeteria.R;
-
-public class MainActivity extends AppCompatActivity {
+public class ActivityMenuDetalles extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu_detalles);
     }
 }
