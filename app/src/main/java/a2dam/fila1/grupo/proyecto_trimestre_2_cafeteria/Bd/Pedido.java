1af5753442a88b2f15e2cc7f37d9da119bbcdead
lalink617 <lalink617@gmail.com>
2017-01-12 11:02:41 +0100

    Bd

---
+++ app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/Bd/Pedido.java
@@ -0,0 +1,21 @@
+package a2dam.fila1.grupo.proyecto_trimestre_2_cafeteria.Bd;
+
+/**
+ * Created by usuario on 12/01/17.
+ */
+
+public class Pedido {
+    int idPedido, idCliente, cantidad;
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+}
