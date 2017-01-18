c97f36fec27368b978a21041ddd7926b76033881
Gand <zaken85@gmail.com>
2017-01-18 22:26:28 +0100

    Gand: Layout cliente

--- app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/ActivityCafe.java
+++ app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/ActivityCafe.java
@@ -1,13 +1,148 @@
 package a2dam.fila1.grupo.proyecto_trimestre_2_cafeteria;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ActivityCafe extends AppCompatActivity {
+
+    Spinner spTipo, spLeche, spAzucar;
+    CheckBox lactosa, crema, chocolate, hielo;
+    TextView precio;
+    ImageButton volver, menu;
+    Button pedir, menos, mas;
+    FloatingActionButton fab;
+    ImageView fotoMenu;
+
+    Boolean leche = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cafe);
+
+        inflar();
+        spiner();
+        listener();
+
+//        llevaLeche(leche);
+
     }
-}
+
+    private void inflar() {
+        spTipo = (Spinner) findViewById(R.id.sp_cf_tipo);
+        spLeche = (Spinner) findViewById(R.id.sp_cf_leche);
+        spAzucar = (Spinner) findViewById(R.id.sp_cf_azucar);
+        lactosa = (CheckBox) findViewById(R.id.cb_cf_lactosa);
+        crema = (CheckBox) findViewById(R.id.cb_cf_crema);
+        chocolate = (CheckBox) findViewById(R.id.cb_cf_choco);
+        hielo = (CheckBox) findViewById(R.id.cb_cf_hielo);
+        precio = (TextView) findViewById(R.id.tv_cf_precioNum);
+        volver = (ImageButton) findViewById(R.id.ib_cf_volver);
+        menu = (ImageButton) findViewById(R.id.ib_cf_menu);
+        pedir = (Button) findViewById(R.id.btn_cf_pedir);
+        menos = (Button) findViewById(R.id.btn_cnt_menos);
+        mas = (Button) findViewById(R.id.btn_cnt_mas);
+        fab = (FloatingActionButton) findViewById(R.id.fab_cf);
+        fotoMenu = (ImageView) findViewById(R.id.iv_cf_menu);
+    }
+
+    private void spiner() {
+        String[] arrayTipo = {"Espreso","Doble","Cortado","Capuchino","Bombón"};
+        spTipo.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayTipo));
+        spTipo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                // vacio
+            }
+        });//Fin Spinner Tipo
+
+        //----------------------------------------------------------------------------------------//
+        //----------------------------------------------------------------------------------------//
+
+        ArrayAdapter<CharSequence> adapterLch = ArrayAdapter.createFromResource(this,
+                R.array.leche, android.R.layout.simple_spinner_item);
+        adapterLch.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spLeche.setAdapter(adapterLch);
+        spLeche.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (parent.getSelectedItemPosition()){
+                    case 0:
+                        break;
+                    default:
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });//Fin Spinner Leche
+
+        //----------------------------------------------------------------------------------------//
+        //----------------------------------------------------------------------------------------//
+
+        ArrayAdapter<CharSequence> adapterAzc = ArrayAdapter.createFromResource(this,
+                R.array.azucar, android.R.layout.simple_spinner_item);
+        adapterAzc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spAzucar.setAdapter(adapterAzc);
+        spAzucar.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (parent.getSelectedItemPosition()){
+                    case 0:
+                        break;
+                    default:
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });//Fin Spinner Leche
+    }
+
+    private void listener() {
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fotoMenu.setVisibility(View.VISIBLE);
+            }
+        });
+
+        //----------------------------------------------------------------------------------------//
+        //----------------------------------------------------------------------------------------//
+
+        fotoMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fotoMenu.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    private void llevaLeche(boolean b) {
+        spLeche.setEnabled(b);
+        lactosa.setEnabled(b);
+    }
+}//Fin Activity
