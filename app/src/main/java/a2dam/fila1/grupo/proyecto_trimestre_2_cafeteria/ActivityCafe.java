6dd99403aaa78fd5a5310c2b1e4c5962fb88fb99
Gand <zaken85@gmail.com>
2017-01-20 18:39:36 +0100

    Gand: Activity Cafe (NO FUNCIONA EL VOLVER)

--- app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/ActivityCafe.java
+++ app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/ActivityCafe.java
@@ -1,6 +1,11 @@
 package a2dam.fila1.grupo.proyecto_trimestre_2_cafeteria;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -24,6 +30,7 @@ public class ActivityCafe extends AppCompatActivity {
     ImageView fotoMenu;
 
     Boolean leche = false;
+    Float p = 0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +70,19 @@ public class ActivityCafe extends AppCompatActivity {
 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                switch (parent.getSelectedItemPosition()){
+                    case 0: p = 1f;     setPrecio();
+                        break;
+                    case 1: p = 1.2f;   setPrecio();
+                        break;
+                    case 2: p = 1.4f;   setPrecio();
+                        break;
+                    case 3: p = 1.6f;   setPrecio();
+                        break;
+                    case 4: p = 1.8f;   setPrecio();
+                        break;
+                    default: p = 1.2f;  setPrecio();
+                }
             }
 
             @Override
@@ -81,21 +100,7 @@ public class ActivityCafe extends AppCompatActivity {
         adapterLch.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spLeche.setAdapter(adapterLch);
-        spLeche.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (parent.getSelectedItemPosition()){
-                    case 0:
-                        break;
-                    default:
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });//Fin Spinner Leche
+        //Fin Spinner Leche
 
         //----------------------------------------------------------------------------------------//
         //----------------------------------------------------------------------------------------//
@@ -105,22 +110,8 @@ public class ActivityCafe extends AppCompatActivity {
         adapterAzc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spAzucar.setAdapter(adapterAzc);
-        spAzucar.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (parent.getSelectedItemPosition()){
-                    case 0:
-                        break;
-                    default:
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });//Fin Spinner Leche
-    }
+        //Fin Spinner Azucar
+    }//Fin Spinner
 
     private void metodosListener() {
         menu.setOnClickListener(new View.OnClickListener() {
@@ -139,10 +130,62 @@ public class ActivityCafe extends AppCompatActivity {
                 fotoMenu.setVisibility(View.INVISIBLE);
             }
         });
+
+        //----------------------------------------------------------------------------------------//
+        //----------------------------------------------------------------------------------------//
+
+        //---NO FUNCIONA :'(--------------------------
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(getApplicationContext());
+                builder1.setMessage("Write your message here.");
+                builder1.setCancelable(false);
+
+                builder1.setPositiveButton(
+                        "Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+            }
+        });
+
+    }//Fin Listener
+
+    private void setPrecio(){
+        float pFinal = p;
+        if (lactosa.isChecked())
+            pFinal += 0.2f;
+        if (hielo.isChecked())
+            pFinal += 0.3f;
+        if (crema.isChecked())
+            pFinal += 0.4f;
+        if (chocolate.isChecked())
+            pFinal += 0.5f;
+
+        precio.setText(""+pFinal);
     }
 
     private void llevaLeche(boolean b) {
         spLeche.setEnabled(b);
         lactosa.setEnabled(b);
+    }
+
+    public void metodosCheked(View view) {
+        setPrecio();
     }
 }//Fin Activity
