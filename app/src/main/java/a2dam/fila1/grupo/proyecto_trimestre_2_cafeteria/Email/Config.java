3ce57eec1b3095cb48e9c7351896cf33b372b0b1
johni <mdam2015mdam@gmail.com>
2017-02-03 13:31:39 +0100

    Fix gmail connect

--- app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/Email/Config.java
+++ app/src/main/java/a2dam/fila1/grupo/proyecto_trimestre_2_cafeteria/Email/Config.java
@@ -5,5 +5,5 @@ package a2dam.fila1.grupo.proyecto_trimestre_2_cafeteria.Email;
  */
 public class Config {
     public static final String EMAIL ="cafeteriamurgi@gmail.com";
-    public static final String PASSWORD ="murgicafeteria@gmail.com";
+    public static final String PASSWORD ="murgicafeteria";
 }
